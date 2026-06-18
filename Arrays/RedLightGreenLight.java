db25fa2955e8e802bbad94a23bfdd47023e230ce
Aman kumar <kumarkaseraaman@gmail.com>
2026-06-18 13:22:43 +0530

    Add CodeChef Red Light Green Light solution (Observation + Counting)
    Implement Red Light, Green Light problem solution.Solved using simple observation and counting approach in O(N) time and O(1) space.

---
+++ Arrays/RedLightGreenLight.java
@@ -0,0 +1,37 @@
+/*
+Problem: Red Light, Green Light
+Platform: CodeChef
+Pattern: Observation + Counting
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T > 0) {
+
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            int count = 0;
+
+            for (int i = 0; i < N; i++) {
+                int h = sc.nextInt();
+
+                if (h > K) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+            T--;
+        }
+    }
+}
